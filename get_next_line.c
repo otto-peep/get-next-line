e442f49941785a52b8a347e40b56b7595829a86f
Pol CONIN <pconin@e2r7p15.42.fr>
2015-12-07 19:50:34 +0100

    firstpushgnl

---
+++ get_next_line.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pconin <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/12/07 19:15:54 by pconin            #+#    #+#             */
+/*   Updated: 2015/12/07 19:50:18 by pconin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdlib.h>
+#include "libft.h"
+
+void	put_line_tab
+
+void		fd_endline(char *str)
+{
+	int ind;
+
+	while (str[ind])
+	{
+		if (str[ind] == 
+		{	
+			find lenght line	
+			put_line_tab
+				}
+
+	}
+}
+
+int		get_next_line(int const fd, char **line)
+{
+	char	*str;
+	int		*ret;
+
+	ret = read(fd, str, BUFF_SIZE);
+
+	return (ret);
+}
