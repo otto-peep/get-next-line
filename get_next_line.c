cce3a08da71911f7ec37bbfff5add73fd4c3dcda
Pol CONIN <pconin@e3r6p13.42.fr>
2016-02-02 16:57:56 +0100

    3 fautes au moulitest

--- get_next_line.c
+++ get_next_line.c
@@ -6,16 +6,32 @@
 /*   By: pconin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/26 15:49:59 by pconin            #+#    #+#             */
-/*   Updated: 2016/01/26 21:08:41 by pconin           ###   ########.fr       */
+/*   Updated: 2016/02/02 16:29:02 by pconin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
 #include "libft.h"
 #include <sys/types.h>
 #include <sys/uio.h>
 #include <stdlib.h>
 #include "get_next_line.h"
 #include <unistd.h>
+
+int		found_newline2(char *tmp)
+{
+	int index;
+
+	index = 0;
+	if (!tmp)
+		return (0);
+	while (tmp[index] && tmp[index] != '\n')
+		index++;
+	if (tmp[index] == '\n')
+		return (index + 1);
+	else
+		return (0);
+}
 
 int		found_newline(char *tmp, int ret)
 {
@@ -50,27 +66,26 @@ void	ft_read(int fd, char **tmp, int *ret)
 
 int		get_next_line(int const fd, char **line)
 {
-	static char *tmp = NULL;
+	static char *tmp;
 	int			ret;
 	char		*mem;
 
 	ret = 0;
-	if (fd < 0)
+	if (fd < 0 || !line)
 		return (-1);
-	while (tmp == NULL || (found_newline(tmp, ret) == 0))
+	while (tmp == NULL || (found_newline(tmp, ret) == 0 && ret >= 0))
 	{
 		ft_read(fd, &tmp, &ret);
 		if (ret <= 0)
-			break;
+			break ;
 	}
 	if (ret == -1)
 		return (-1);
 	*line = ft_strsub(tmp, 0, found_newline(tmp, ret));
+	if (found_newline2(tmp) == 0 && ret == 0 && !line[0][0])
+		return (0);
 	mem = tmp;
 	tmp = ft_strsub(tmp, found_newline(tmp, ret) + 1, ft_strlen(tmp));
 	free(mem);
-	if (found_newline(tmp, ret) == 0 && ret == 0)
-		return (0);
-	else
-		return (1);
+	return (1);
 }
